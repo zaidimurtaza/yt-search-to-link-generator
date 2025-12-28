eae7528a910615d817187177590c07921c17081b
Murtaza Zaidi <zaidimurtaza102@gmail.com>
2025-12-28 16:42:16 +0530

    fix: added Procfile

--- run.py
+++ run.py
@@ -58,4 +58,6 @@ def delete(filename):
         return jsonify({"message": "Error deleting song", "status": "error", "get_remote_address": get_remote_address(), "error": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
